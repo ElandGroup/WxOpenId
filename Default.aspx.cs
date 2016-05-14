52a229d94e876dfb465ba86b48c1c87411557b54
xiaoxinmiao <xiaoxm_001@163.com>
2016-05-14 15:34:29 +0800

    modify path

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -8,8 +8,10 @@ namespace WxOpenId
         {
             string openIdProcessUrl = "http://wxdemo.accurate9.com/OpenIdProcess.aspx";
             string targetUrl = "http://wxdemo.accurate9.com/UserInfo.aspx";
-            string url = string.Format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfc82543f2ac6&redirect_uri={0}?reurl={1}/"+
-                                        "&response_type=code&scope=snsapi_base&state=1#wechat_redirect",openIdProcessUrl,targetUrl);
+           
+            string url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" 
+                + CommonHelper.AppId + "&redirect_uri=http://wxdemo.accurate9.com/OpenIdProcess.aspx?reurl=" 
+                + targetUrl + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
             Response.Redirect(url);
         }
     }
